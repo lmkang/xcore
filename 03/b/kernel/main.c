7198ad1c13f7b9b8425a671320e2d48c03a172f9
lmkang <lmkang25@163.com>
2018-09-19 14:48:10 +0800

    03/b-add put_str

--- 03/b/kernel/main.c
+++ 03/b/kernel/main.c
@@ -1,13 +1,6 @@
 #include "print.h"
 int main() {
-	put_char('k');
-	put_char('e');
-	put_char('r');
-	put_char('n');
-	put_char('e');
-	put_char('l');
-	put_char('!');
-	put_char('\n');
+	put_str("hello,kernel!\n");
 	while(1) {
 		
 	}
--- 03/b/lib/kernel/print.h
+++ 03/b/lib/kernel/print.h
@@ -2,4 +2,5 @@
 #define __LIB_KERNEL_PRINT_H
 #include "../stdint.h"
 void put_char(uint8_t ch);
+void put_str(char *str);
 #endif
