bd0b3ab5eb5f7629ce5e1fa13daca474fffa58d6
lmkang <lmkang25@163.com>
2018-09-19 14:19:43 +0800

    fix warn bug

---
+++ 03/b/kernel/main.c
@@ -0,0 +1,14 @@
+#include "print.h"
+int main() {
+	put_char('k');
+	put_char('e');
+	put_char('r');
+	put_char('n');
+	put_char('e');
+	put_char('l');
+	put_char('!');
+	put_char('\n');
+	while(1) {
+		
+	}
+}
