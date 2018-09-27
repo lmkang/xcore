276d75a3c75634c31da2ea3d20cd4270e7e0ac68
lmkang <35437957+lmkang@users.noreply.github.com>
2018-09-27 11:09:55 +0800

    Update run.bat

--- 05/c/run.bat
+++ 05/c/run.bat
@@ -1,5 +1,5 @@
-copy build\xcore.img D:\Tools\Bochs-2.6.8\dlxlinux\xcore.img
+copy build\xcore.img D:\Bochs-2.6.8\dlxlinux\xcore.img
 D:
-cd D:\Tools\Bochs-2.6.8\dlxlinux
+cd D:\Bochs-2.6.8\dlxlinux
 ..\bochsdbg.exe -q -f bochsrc.bxrc
 cls
