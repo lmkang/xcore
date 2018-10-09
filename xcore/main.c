4f6335ac58aff9cd7d7be082d87ab83a26a777f7
lmkang <lmkang25@163.com>
2018-10-10 07:33:49 +0800

    update

--- xcore/main.c
+++ xcore/main.c
@@ -4,40 +4,31 @@
 #include "multiboot.h"
 
 #define CHECK_FLAG(flag, bit) ((flag) & (1 << (bit)))
+#define TOTAL_MEM_SIZE_ADDR 0x90000
 
-void show_mmap(struct multiboot *mboot_ptr) {
-	uint32_t *mem_size_addr = (uint32_t *) 0x90000;
-	if (CHECK_FLAG(mboot_ptr->flags, 0)) {
-        *mem_size_addr = (mboot_ptr->mem_lower * 1024) + (mboot_ptr->mem_upper * 1024);
-        put_str("Total Memory: ");
-        put_hex((*mem_size_addr) / (1024 * 1024) + 1);
-		put_str("MB\n");
-    }
-	if(CHECK_FLAG(mboot_ptr->flags, 6)) {
-		uint32_t mmap_addr = mboot_ptr->mmap_addr;
-		uint32_t mmap_length = mboot_ptr->mmap_length;
-		struct mmap_entry *mmap = (struct mmap_entry*) mmap_addr;
-		for(; (uint32_t) mmap < mmap_addr + mmap_length; mmap++) {
-			// base_addr_low + length_low
-			put_str("mmap->base_addr_low: ");
-			put_hex((uint32_t) mmap->base_addr_low);
-			put_char('\n');
-			put_str("mmap->length_low: ");
-			put_hex((uint32_t) mmap->length_low);
-			put_char('\n');
-		}
-	}
-}
+void get_total_mem(struct multiboot *mboot_ptr);
 
 int main(struct multiboot *mboot_ptr) {
+	// 初始化所有模块
 	init_all();
+	
+	// 获取总物理内存容量,存在TOTAL_MEM_SIZE_ADDR地址处
+	get_total_mem(mboot_ptr);
+	
+	// 打印总物理内存容量
+	put_hex(*((uint32_t *) TOTAL_MEM_SIZE_ADDR));
+	put_str("MB\n");
 	
 	//enable_intr();
 	//__asm__ __volatile__("int $20");
 	
-	put_str("hello, kernel!\n");
-	
-	show_mmap(mboot_ptr);
-	
 	return 0;
+}
+
+// 获取总物理内存容量,存在TOTAL_MEM_SIZE_ADDR地址处
+void get_total_mem(struct multiboot *mboot_ptr) {
+	uint32_t *mem_size_addr = (uint32_t *) TOTAL_MEM_SIZE_ADDR;
+	if (CHECK_FLAG(mboot_ptr->flags, 0)) {
+        *mem_size_addr = ((mboot_ptr->mem_lower * 1024) + (mboot_ptr->mem_upper * 1024)) / (1024 * 1024) + 1;
+    }
 }
