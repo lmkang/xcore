aaa68453f1e10a927fb754f3818df902af462e44
lmkang <lmkang25@163.com>
2018-10-11 01:03:47 +0800

    update page

--- xcore/init.c
+++ xcore/init.c
@@ -1,7 +1,9 @@
 #include "gdt.h"
 #include "interrupt.h"
+#include "page.h"
 
 void init_all() {
 	init_gdt(); // 初始化GDT
 	init_idt(); // 初始化IDT
+	init_page(); // 初始化分页
 }
--- xcore/page.c
+++ xcore/page.c
@@ -3,15 +3,36 @@
 
 // page
 #define PDT_BASE_ADDR 0x400000 // page diretory table base address
+#define PAGE_ATTR 0x7 // 页属性
+
+extern void flush_page();
+
+// 创建页目录表和页表
+static void setup_page() {
+	// 1 将页目录表(PDT)清0
+	memset((void*) PDT_BASE_ADDR, 0, 4096);
+	// 2 创建页目录项(PDE)
+	// 将0xc03fffff以下的地址和0x003fffff以下的地址都指向相同的页表
+	uint32_t *pde = (uint32_t*) PDT_BASE_ADDR;
+	*pde = (PDT_BASE_ADDR + 0x1000) | PAGE_ATTR;
+	*(pde + 0xc00) = (PDT_BASE_ADDR + 0x1000) | PAGE_ATTR;
+	*(pde + 4092) = PDT_BASE_ADDR | PAGE_ATTR;
+	// 3 创建页表项(PTE)
+	uint32_t *pte = (uint32_t*) (PDT_BASE_ADDR + 0x1000);
+	for(int i = 0; i < 1024; i++) {
+		*(pte + i * 4) = 4096 * i + PAGE_ATTR;
+	}
+	// 4 创建内核其他页表的PDE
+	// 范围是769-1022的所有页目录项
+	pde = (uint32_t*) PDT_BASE_ADDR;
+	int index = 0;
+	for(int i = 769; i < 1023; i++) {
+		*(pde + i * 4) = (PDT_BASE_ADDR + 0x2000 + PAGE_ATTR) + (index++) * 0x1000;
+	}
+}
 
 // 初始化分页
 void init_page() {
-	// 1 将页目录表清0
-	memset((void*) PDT_BASE_ADDR, 0, 4096);
-	// 2 创建页目录项
-	// 将0xc03fffff以下的地址和0x003fffff以下的地址都指向相同的页表
-	uint32_t *pde = (uint32_t*) PDT_BASE_ADDR;
-	*pde = (PDT_BASE_ADDR + 0x1000) | 0x7;
-	*(pde + 0xc00) = (PDT_BASE_ADDR + 0x1000) | 0x7;
-	*(pde + 4092) = PDT_BASE_ADDR | 0x7;
+	setup_page();
+	flush_page();
 }
